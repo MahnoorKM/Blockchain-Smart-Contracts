2dffbe685628adda1a6f2003118497e4ed1aa289
MahnoorKM <bcsbs2112310@szabist.pk>
2025-02-27 12:53:00 +0500

    erc20

---
+++ contracts/lecture_02/ERC20.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract MyToken is ERC20, ERC20Permit {
+    constructor() ERC20("Mahnoor", "MTK") ERC20Permit("Mahnoor") {}
+}

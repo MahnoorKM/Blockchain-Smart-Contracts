1e713e308c24f1971967671cf864c0e72be948fe
MahnoorKM <bcsbs2112310@szabist.pk>
2025-03-06 11:05:32 +0500

    lect3

--- contracts/lecture_03/dummy_token.sol
+++ contracts/lecture_03/dummy_token.sol
@@ -5,6 +5,9 @@ pragma solidity ^0.8.22;
 contract dummyToken {
 
     mapping (address => uint) public balanceOf;
+    uint public totalSupply;
+    uint public mintCap = 1000;
+    address owner = msg.sender;
 
     function deposit(uint amount) public {
         balanceOf[msg.sender] = amount;
@@ -17,6 +20,15 @@ contract dummyToken {
         balanceOf[recipient] += amount;
 
         return true;
+    }
+
+     function mint(address to, uint amount) public {
+        require(msg.sender == owner, "Only the owner is allowed to mint");
+        require(to != address(0), "Cannot mint to zero address");
+        require(amount > 0, "Cannot mint zero or less than zero");
+        require(amount <= mintCap, "Mint limit exceeded");
+        balanceOf[msg.sender] += amount;  // Add newly minted tokens to recipient's balance
+        totalSupply += amount;    // Increase total supply
     }
 
 }
